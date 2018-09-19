d78b234a966fb4772d55c8bd206c0955361c3dc9
liangwuqiang <l_wuqiang@126.com>
2018-09-19 20:35:46 +0800

    3

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from scrapy import cmdline
 # cmdline.execute("scrapy crawl xici -o xici.csv".split())
 
 
-cmdline.execute("scrapy crawl tm_goods".split())
+cmdline.execute("scrapy crawl tm_goods -o results.csv".split())
