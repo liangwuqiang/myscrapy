66234a47f47c8ad33117aedd9c671552060d3668
liangwuqiang <l_wuqiang@126.com>
2018-09-27 15:02:19 +0800

    13

--- wang_yi_yun.py
+++ wang_yi_yun.py
@@ -1,79 +1,57 @@
-# -*- coding: utf-8 -*-
-
 from selenium import webdriver
+from bs4 import BeautifulSoup
 import csv
 
-# url = 'file:///home/ubuntu/Scrapy/example/test.html'
-url = 'https://music.163.com/#/discover/playlist'  # 用于测试
 
-# 无界面调用‘火狐’浏览器的方法
-options = webdriver.FirefoxOptions()
-options.add_argument('-headless')
-driver = webdriver.Firefox(options=options)
-# print(help(webdriver.Firefox))  # 查看方法的配置信息
-driver.get(url)
+class WangYiYun(object):
+    url = ''
 
-csv_file = open('playlist.csv', 'w', newline='')
-writer = csv.writer(csv_file)
-writer.writerow(['标题', '播放数', '链接'])
+    def __init__(self, url):
+        self.url = url
+
+    def run(self):
+        csv_file = open('playlist.csv', 'w', newline='')
+        writer = csv.writer(csv_file)
+        writer.writerow(['标题', '播放数', '链接'])
+
+        i = 0
+        url = self.url
+        while url != 'javascript:void(0)':
+            i = i+1
+            print('调试', i)
+            print(url)
+
+            # 无界面调用‘火狐’浏览器的方法
+            options = webdriver.FirefoxOptions()
+            options.add_argument('-headless')
+            driver = webdriver.Firefox(options=options)
+            # print(help(webdriver.Firefox))  # 查看方法的配置信息
+
+            driver.get(url)  # 全局的内容
+            driver.switch_to.frame("contentFrame")  # 局部的内容
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+            divs = soup.select('.u-cover.u-cover-1')
+
+            for div in divs:
+                nb = div.select('.nb')[0].text
+                if '万' in nb and int(nb.split("万")[0]) > 500:
+                    title = div.select('.msk')[0]['title']
+                    link = 'https://music.163.com/#' + div.select('.msk')[0]['href']
+                    # 样例 https://music.163.com/#/playlist?id=2065883262
+                    print(title, nb, link)
+                    writer.writerow([title, nb, link])
+
+            url = 'https://music.163.com/#' + soup.select('a.zbtn.znxt')[0]['href']
+        csv_file.close()
 
 
-driver.switch_to.frame("contentFrame")
-# data = driver.find_element_by_id("m-pl-container").find_element_by_tag_name("li")
-data = driver.find_element_by_xpath('//*[@id="m-pl-container"]')
-data.
-print(driver.page_source)
-with open('temp.html', 'w') as f:
-    f.write(driver.page_source)
-# print(len(data))
-# for i in range(len(data)):
-#     print(i)
-    # 获取播放数
-    # nb = data[i].find_element_by_class_name("nb").text
-    # if '万' in nb and int(nb.split("万")[0]) > 500:
-    #     msk = data[i].find_element_by_css_selector("a.msk")
-    #     writer.writerow([msk.get_attribute("title"), nb, msk.get_attribute("href")])
+def main():
+    start_url = 'https://music.163.com/#/discover/playlist'
+    crawler = WangYiYun(start_url)
+    crawler.run()
+    print('完成')
 
 
-csv_file.close()
-
-# data = driver.title
-# print(data)
-print('结束')
-
-def temp():
-
-    # 网易云音乐第一页的地址
-    #
-    url = r'http://music.163.com/#/discover/playlist/?order=hot&cat=%E5%85%A8%E9%83%A8&limit=35&offset=0'
-
-
-    # 准备好存储歌单的csv文件
-    csv_file = open('playlist.csv', 'w', newline='')
-    writer = csv.writer(csv_file)
-    writer.writerow(['标题', '播放数', '链接'])
-
-    # 解析每一页，直到下一页为空
-    while url != 'javascript:void(0)':
-        # 用WebDriver加载画面
-        driver.get(url=url)
-        # 切换到内容的iframe
-        driver.switch_to.iframe("contentFrame")
-        # 定位歌单标签
-        data = driver.find_element_by_id("m-pl-container"). \
-            find_element_by_tag_name("li")
-
-        # 解析一页中的所有歌单
-        for i in range(len(data)):
-            # 获取播放数
-            nb = data[i].find_element_by_class_name("nb").text
-            if '万' in nb and int(nb.split("万")[0]) > 500:
-                # 获取播放数大于500万的歌曲封面
-                msk = data[i].find_element_by_css_selector("a.msk")
-                # 把封面上的标题和链接播放数写到同一个文件中
-                writer.writerow([msk.get_attribute("title"), nb, msk.get_attribute("href")])
-
-        # 定位下一页
-        url = driver.find_element_by_css_selector("a.zbtn.znxt").get_attribute('href')
-
-    csv_file.close()
+if __name__ == '__main__':
+    main()
